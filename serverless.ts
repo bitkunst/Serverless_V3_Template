090095641db7b5768e922f64829261f0944a2c2d
bitkunst <bitkunst23@gmail.com>
2026-01-05 12:35:33 +0900

    update: serverless.ts & package.json

--- serverless.ts
+++ serverless.ts
@@ -1,35 +1,54 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import hello from "@functions/hello";
 
-const serverlessConfiguration: AWS = {
-  service: 'serverless-v3-template',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+const serverlessConfiguration: Omit<AWS, "provider"> & {
+  provider: Omit<AWS["provider"], "vpc"> & {
+    vpc: { securityGroupIds: string; subnetIds: string };
+  };
+} = {
+  service: "serverless-v3-template",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild", "serverless-offline"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
+    name: "aws",
+    runtime: "nodejs20.x",
+    architecture: "arm64",
+    region: "ap-northeast-2",
+    stage: "dev",
+    logRetentionInDays: 14,
+    deploymentBucket: "",
+    stackName: "${self:service}-stack-${sls:stage}",
+    httpApi: {
+      id: "",
+    },
+    vpc: {
+      securityGroupIds: "",
+      subnetIds: "",
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
+
   // import the function via paths
   functions: { hello },
-  package: { individually: true },
+
+  package: {
+    individually: true,
+    excludeDevDependencies: true,
+  },
+
   custom: {
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node20",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
